1614a2221481079d30b86bc5d59f4e2f3619d952
aathirajan <saathirajan99@gmail.com>
2023-01-09 13:50:42 +0530

    portfoliocss

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -65,12 +65,12 @@ const Portfolio = () => {
   ]
   return (
     <div name='portfolio' className='bg-[#00171F] w-full pb-5 text-white md:h-fit  '>
-      <div className=' max-w-screen-lg  mx-auto flex flex-col justify-center w-full h-full'>
+      <div className=' max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
         <div className=' pb-8'>
           <p className=' text-4xl font-bold inline border-b-4  border-gray-500'>Portfolio</p>
           <p className='font-signature py-6'>Check some of my work right here.</p>
         </div>
-        <div className=' grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
+        <div className=' grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-8 sm:px-0'>
         {
           portfolios.map(({id, src, name, tech, ghLink}) => (
             
