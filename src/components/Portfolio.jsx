0f09e0501ab4fffbd234694e8acb2aa4dcebb8ae
aathirajan <saathirajan99@gmail.com>
2023-01-09 13:18:29 +0530

    responsive commit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -65,7 +65,7 @@ const Portfolio = () => {
   ]
   return (
     <div name='portfolio' className='bg-[#00171F] w-full pb-5 text-white md:h-fit  '>
-      <div className=' max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+      <div className=' max-w-screen-lg  mx-auto flex flex-col justify-center w-full h-full'>
         <div className=' pb-8'>
           <p className=' text-4xl font-bold inline border-b-4  border-gray-500'>Portfolio</p>
           <p className='font-signature py-6'>Check some of my work right here.</p>
